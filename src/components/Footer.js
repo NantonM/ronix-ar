3d11e09aacf12a8dc410bee36474eb87496f106a
Matias <matias.nanton@gmail.com>
2025-05-13 17:20:53 -0300

    Implementación inicial del nuevo diseño del Header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -72,15 +72,16 @@ export default function Footer() {
                 CONTACTO
               </h6>
               <p className={styles.footerText}>
-                <FontAwesomeIcon icon={faEnvelope} size="xs" className="me-2" /> {/* Añadido size="sm" */}
-              <a href="mailto:ronix@ixnova.com.ar" className={styles.footerLinkNoBlock}>ronix@ixnova.com.ar</a>
+                {/* Aplicando la clase y quitando me-2 si el margen está en CSS */}
+                <FontAwesomeIcon icon={faEnvelope} className={styles.contactIcon} />
+                <a href="mailto:ronix@ixnova.com.ar" className={styles.footerLinkNoBlock}>ronix@ixnova.com.ar</a>
               </p>
               <p className={styles.footerText}>
-                <FontAwesomeIcon icon={faPhone} size="xs" className="me-2" /> {/* Añadido size="sm" */}
+                <FontAwesomeIcon icon={faPhone} className={styles.contactIcon} />
                 <a href="tel:+5491168261600" className={styles.footerLinkNoBlock}>+54 9 11 6826 1600</a>
               </p>
               <p className={styles.footerText}>
-                <FontAwesomeIcon icon={faMapMarkerAlt} size="xs" className="me-2" /> {/* Añadido size="sm" */}
+                <FontAwesomeIcon icon={faMapMarkerAlt} className={styles.contactIcon} />
                 Salvador Curutchet 1747 - Castelar - Buenos Aires - Argentina
               </p>
               <h6 className={`text-uppercase fw-bold mb-4 ${styles.footerTitle}`}>
